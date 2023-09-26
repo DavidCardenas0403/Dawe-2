cc29b3dde207d97489d80aac5e7b556240f25a04
DavidCardenas0403 <davidcardenasrafael03@gmail.com>
2023-09-26 13:33:59 +0200

    Adding comments to the functions

--- m14/contrasenya.ts
+++ m14/contrasenya.ts
@@ -2,12 +2,15 @@ let contrasenya: string | null = (
     document.getElementById("contrasenya") as HTMLInputElement
 ).value;
 
+//Funció principal que crida a la resta de funcions per comprovar si la contrasenya compleix tots els requeriments
 const comprovarContrasenya = () => {};
 
+//Funció que comprova que la contrasenya tingui entre 8 i 20 caracters (ambdós inclosos)
 const comprovarLlargada = (contrasenya: string): boolean => {
     return contrasenya.length >= 8 && contrasenya.length <= 20;
 };
 
+//Funció que comprova que la contrasenya tingui com a mínim 3 minúscules, 1 majúscula, 2 dígits i que no tingui 3 caracters iguals seguits
 const comprovarCaracters = (contrasenya: string) => {
     let minuscules: number = 0;
     let majuscules: number = 0;
