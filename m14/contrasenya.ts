f33c335845a25549590db91be7efe9c1ff46319d
DavidCardenas0403 <david.cardenas.7e6@itb.cat>
2023-09-26 20:04:40 +0200

    contrasenya casi acabada

--- m14/contrasenya.ts
+++ m14/contrasenya.ts
@@ -1,9 +1,17 @@
+let contrasenyaValida: boolean = true;
+let mensaje: string[] = [];
+
 let contrasenya: string | null = (
     document.getElementById("contrasenya") as HTMLInputElement
 ).value;
 
 //Funció principal que crida a la resta de funcions per comprovar si la contrasenya compleix tots els requeriments
-const comprovarContrasenya = () => {};
+const comprovarContrasenya = () => {
+    if (contrasenya && !comprovarLlargada(contrasenya)) {
+        mensaje.push("La contrasenya ha de tenir entre 8 i 20 caracters");
+    } else {
+    }
+};
 
 //Funció que comprova que la contrasenya tingui entre 8 i 20 caracters (ambdós inclosos)
 const comprovarLlargada = (contrasenya: string): boolean => {
@@ -25,5 +33,24 @@ const comprovarCaracters = (contrasenya: string) => {
         } else if (/[0-9]/.test(contrasenya[index])) {
             digits++;
         }
+
+        if (contrasenya[index] === contrasenya[index - 1]) {
+            caractersSeguits++;
+        } else {
+            caractersSeguits = 1;
+        }
+
+        if (caractersSeguits === 3) {
+            contrasenyaValida = false;
+            mensaje.push(
+                "La contrasenya no pot tenir 3 caracters iguals seguits"
+            );
+            return;
+        }
+    }
+
+    if (minuscules < 3 || majuscules < 1 || digits < 2) {
+        contrasenyaValida = false;
+        return "La contrasenya no compleix tots els requisits";
     }
 };
