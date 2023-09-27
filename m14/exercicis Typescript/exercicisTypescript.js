f3070ecca0168f755888ad3886099def5e89918f
DavidCardenas0403 <davidcardenasrafael03@gmail.com>
2023-09-27 10:51:33 +0200

    exercicis typescript fins al 5

---
+++ m14/exercicis Typescript/exercicisTypescript.js
@@ -0,0 +1,38 @@
+//Execici 1
+var sumaDeNombres = function (array) {
+    return array.reduce(function (acumulador, num) { return acumulador + num; });
+};
+console.log("Exercici 1: " + sumaDeNombres([1, 2, 3, 4, 5]));
+//Exercici 2
+var filtrarNombresParells = function (array) {
+    return array.filter(function (num) { return num % 2 === 0; });
+};
+console.log("Exercici 2: " + filtrarNombresParells([0, 1, 3, 5, 8, 12, 21, 22]));
+//Exercici 3
+var comptarParaules = function (frase) {
+    return frase.split(" ").length;
+};
+console.log("Exercici 3: " + comptarParaules("Esta frase tiene cinco palabras"));
+//Exercici 4
+var invertirCadena = function (cadena) {
+    return cadena.split("").reverse().join("");
+};
+console.log("Exercici 4: " + invertirCadena("hola"));
+//Exercici 5
+var calcularFactorial = function (num) {
+    var numero = parseInt(num.toFixed(0));
+    var factorial = 1;
+    if (numero < 0) {
+        throw new Error("El número tiene que ser mayor o igual que 0");
+    }
+    else if (numero === 0) {
+        return 1;
+    }
+    else {
+        for (var i = 1; i <= numero; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+};
+console.log("Exercici 5: " + calcularFactorial(5));
---
+++ m14/exercicis Typescript/exercicisTypescript.ts
@@ -0,0 +1,49 @@
+//Execici 1
+const sumaDeNombres = (array: number[]): number => {
+    return array.reduce((acumulador, num) => acumulador + num);
+};
+
+console.log("Exercici 1: " + sumaDeNombres([1, 2, 3, 4, 5]));
+
+//Exercici 2
+const filtrarNombresParells = (array: number[]): number[] => {
+    return array.filter((num) => num % 2 === 0);
+};
+
+console.log(
+    "Exercici 2: " + filtrarNombresParells([0, 1, 3, 5, 8, 12, 21, 22])
+);
+
+//Exercici 3
+const comptarParaules = (frase: string): number => {
+    return frase.split(" ").length;
+};
+
+console.log(
+    "Exercici 3: " + comptarParaules("Esta frase tiene cinco palabras")
+);
+
+//Exercici 4
+const invertirCadena = (cadena: string): string => {
+    return cadena.split("").reverse().join("");
+};
+
+console.log("Exercici 4: " + invertirCadena("hola"));
+
+//Exercici 5
+const calcularFactorial = (num: number): number => {
+    const numero = parseInt(num.toFixed(0));
+    let factorial = 1;
+    if (numero < 0) {
+        throw new Error("El número tiene que ser mayor o igual que 0");
+    } else if (numero === 0) {
+        return 1;
+    } else {
+        for (let i = 1; i <= numero; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+};
+
+console.log("Exercici 5: " + calcularFactorial(5));
