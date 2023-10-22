c12534c7b8816f38876c006af04839a4caf9d855
DavidCardenas0403 <davidcardenasrafael03@gmail.com>
2023-10-22 12:16:47 +0200

    eliminar proyecto pokemons

--- m14/pokemons/src/components/DetallsPokemon.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-    <h2>Detall</h2>
-</template>
-
-<style scoped></style>
--- m14/pokemons/src/components/LlistaPokemon.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-    <h2>Llista</h2>
-</template>
-
-<style scoped></style>
