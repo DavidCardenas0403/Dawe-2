51c5c2ed55d361297cbc1d5a7b2e560bf3a3af6d
DavidCardenasITB <david.cardenas.7e6@itb.cat>
2023-11-21 20:02:54 +0100

    empezar top-experiences

---
+++ m14/top-experiences/src/components/TheNavigation.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/brazil">Brazil</RouterLink>
+    <RouterLink to="/panama">Panama</RouterLink>
+    <RouterLink to="/hawaii">Hawaii</RouterLink>
+    <RouterLink to="/jamaica">Jamaica</RouterLink>
+  </nav>
+</template>
+
+<style scoped></style>
--- m14/top-experiences/src/router/index.ts
+++ m14/top-experiences/src/router/index.ts
@@ -1,23 +1,44 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import DestinationShow from "../views/DestinationShow.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/destination/:id",
+      name: "destination.show",
+      component: DestinationShow,
+    },
+    {
+      path: "/destination/:id/:slug",
+      name: "destination.show",
+      component: DestinationShow,
+      props: (route) => ({ ...route.params, id: parseInt(route.params.id[0]) }),
 
-export default router
+      children: [
+        {
+          path: ":experienceSlug",
+          name: "experience.show",
+          component: () => import("../views/ExperienceShow.vue"),
+        },
+      ],
+    },
+    {
+      path: "/destination/:id",
+      name: "destination.show",
+      component: DestinationShow,
+    },
+  ],
+});
+
+export default router;
