a072a533a82eb0eeed1948097ef12854d095f1c2
DavidCardenasITB <david.cardenas.7e6@itb.cat>
2023-11-28 20:02:11 +0100

    arreglar error destination show

--- m14/top-experiences/src/views/DestinationShow.vue
+++ m14/top-experiences/src/views/DestinationShow.vue
@@ -2,29 +2,28 @@
 import type Destination from "@/types/Destination";
 import data from "../../data/data.json";
 import router from "@/router";
-import { ref, type Ref, reactive } from "vue";
+import { ref, type Ref, computed } from "vue";
 import type { RouteParams } from "vue-router";
 
 const props = defineProps(["id"]);
 
+const destination = computed(() => {
+  return data.destinations.find((destination) => destination.id === props.id);
+});
+
 const params: RouteParams = router.currentRoute.value.params;
-const destination: Ref<Destination | null> = ref(null);
+//const destination: Ref<Destination | null> = ref(null);
 
-const fetchData = reactive(
-  (data: { destinations: Array<Destination> }): void => {
-    destination.value = data.destinations.filter(
-      (destination) =>
-        destination.id === parseInt(params?.id as string) &&
-        destination.slug === params.slug.toString()
-    )[0];
-  }
-);
-
+/* const fetchData = (data: { destinations: Array<Destination> }): void => {
+  destination.value = data.destinations.filter(
+    (destination) =>
+      destination.id === parseInt(params?.id as string) &&
+      destination.slug === params.slug.toString()
+  )[0];
+}; */
 const goBack = (): void => {
   router.back();
 };
-
-fetchData(data);
 </script>
 
 <template>
