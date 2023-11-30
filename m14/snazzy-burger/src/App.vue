316ff0c6ab26e3001775141f9cc32c41da876fa8
DavidCardenasITB <david.cardenas.7e6@itb.cat>
2023-11-30 18:40:56 +0100

    empezar proyecto snazzy burger

---
+++ m14/snazzy-burger/src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref, type Ref, reactive } from "vue";
+const orderName: Ref<string> = ref("The Snazzy Burger");
+const products = reactive([
+  { name: "Hamburger 🍔.", price: 5 }, //Este precio es en euros (€)
+  { name: "Cheeseburger 🧀", price: 6 },
+  { name: "Impossible Burger 🥕", price: 7 },
+  { name: "Fries 🍟", price: 2 },
+]);
+</script>
+
+<template>
+  <header>
+    <h1>{{ orderName }}</h1>
+
+    <section>
+      <input v-model="orderName" type="text" />
+      <button type="button">Place Order</button>
+    </section>
+  </header>
+
+  <main>
+    <section>
+      <Comanda
+        v-for="(product, index) in products"
+        :info="product"
+        :key="index"
+      />
+    </section>
+  </main>
+</template>
+
+<style scoped>
+header,
+section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+}
+</style>
---
+++ m14/snazzy-burger/src/components/Comanda.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type Product from "../../types/Product.ts"
+  defineProps<{
+    info:
+  }>()
+</script>
+
+<template></template>
+
+<style scoped>
+article {
+  display: flex;
+  gap: 5px;
+  flex-wrap: wrap;
+}
+</style>
