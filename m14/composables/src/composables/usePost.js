ab58ace651d87d83c6e37b60cd19980a5f137e23
DavidCardenasITB <david.cardenas.7e6@itb.cat>
2023-12-13 18:45:26 +0100

    composables casi acabada

---
+++ m14/composables/src/composables/usePost.js
@@ -0,0 +1,27 @@
+import { ref } from "vue";
+
+export default function usePost() {
+  const posts = ref([]);
+  const post = ref(null);
+  const baseUrl = "https://jsonplaceholder.typicode.com/";
+  /* 
+  2 - La funció fetch
+  3 - Una variable reactiva que guardi tots els posts, és un array d'objectes. 
+  Ha de ser reactiva perquè si canvia la url, també cambiaran els posts (ja que es crida a una altre a url amb el fetch)
+  5 - Fa que la funció fetch sigui síncrona i no asíncrona, és a dir, espera a que la crida a la API es faci completament, i després continua executant 
+  la resta del codi. Si fos asíncrona continuaria amb executant el codi, encara que la crida a la API no hagués acabat.
+  */
+
+  const llegirPosts = async () => {
+    const response = await fetch(baseUrl + "posts");
+    posts.value = await response.json();
+  };
+
+  const llegirPost = async (id) => {
+    const response = await fetch(baseUrl + "posts/" + id);
+    post.value = await response.json();
+    console.log(post.value);
+  };
+
+  return { posts, llegirPosts, post, llegirPost };
+}
---
+++ m14/composables/src/views/Post.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="post && user">
+    <h1 class="text-3xl">{{ post.title }}</h1>
+    <div class="text-gray-500 mb-10">by {{ user.name }}</div>
+    <div>{{ post.body }}</div>
+  </div>
+</template>
+<script setup>
+import { useRoute } from "vue-router";
+import usePost from "../composables/usePost.js";
+import useUser from "../composables/useUser.js";
+const { llegirPost, post } = usePost();
+const { llegirUser, user } = useUser();
+const router = useRoute();
+
+const getPost = async () => {
+  await llegirPost(router.params.id);
+};
+
+getPost();
+
+llegirUser(post.value.userId);
+</script>
