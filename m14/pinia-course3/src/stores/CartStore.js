00b2cae4b9212f00771d386284987191b1845e6d
DavidCardenas0403 <david.cardenas.7e6@itb.cat>
2024-01-13 13:11:16 +0100

    practica pinia 3 acabada

--- m14/pinia-course3/src/stores/CartStore.js
+++ m14/pinia-course3/src/stores/CartStore.js
@@ -1,30 +1,36 @@
-import { defineStore } from 'pinia'
-import { groupBy } from 'lodash'
+import { defineStore } from "pinia";
+import { groupBy, orderBy } from "lodash";
 
-export const useCartStore = defineStore('CartStore', {
+export const useCartStore = defineStore("CartStore", {
   state: () => {
     return {
       items: [],
-    }
+    };
   },
   getters: {
     count: (state) => state.items.length,
     isEmpty: (state) => state.count === 0,
-    grouped: (state) => groupBy(state.items, (item) => item.name),
+    grouped: (state) => {
+      const orderedItems = orderBy(state.items, (item) => item.name, "asc");
+      return groupBy(orderedItems, (item) => item.name);
+    },
     groupCount: (state) => (name) => state.grouped[name].length,
     //El getter "totalPrice" recorre todos los productos que hay en el array "items" del store y suma los precios de todos los elementos
     totalPrice: (state) => state.items.reduce((acc, b) => acc + b?.price, 0),
   },
   actions: {
     addItems(count, item) {
-      count = parseInt(count)
+      count = parseInt(count);
       for (let index = 0; index < count; index++) {
-        this.items.push({ ...item })
+        this.items.push({ ...item });
       }
     },
     clearItem(name) {
-      this.items = this.items.filter((item) => item.name != name)
+      this.items = this.items.filter((item) => item.name != name);
     },
-    setItemCount(item, count) {},
+    setItemCount(item, count) {
+      this.clearItem(item.name);
+      this.addItems(count, item);
+    },
   },
-})
+});
