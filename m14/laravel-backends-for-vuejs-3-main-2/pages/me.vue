6530a55f2f5a6a323d9d41904256583648b240e5
DavidCardenasITB <david.cardenas.7e6@itb.cat>
2024-02-08 18:56:07 +0100

    empezar practica2 laravel

---
+++ m14/laravel-backends-for-vuejs-3-main-2/pages/me.vue
@@ -0,0 +1,29 @@
+<script setup>
+import axios from "axios";
+definePageMeta({
+  middleware: ["auth"],
+});
+
+const name = ref("");
+const email = ref("");
+
+const getUserInfo = async () => {
+  let resUserInfo;
+  try {
+    resUserInfo = await axios.get("/user");
+    console.log(resUserInfo.data);
+    name.value = resUserInfo.data?.name;
+    email.value = resUserInfo.data?.email;
+    console.log(name.value, email.value);
+  } catch (e) {
+    console.log(`Ha ocurrido un error: ${e}`);
+  }
+  console.log(resUserInfo);
+};
+
+getUserInfo();
+</script>
+<template>
+  <p><strong>User Name:</strong> {{ name }}</p>
+  <p><strong>User Email:</strong> {{ email }}</p>
+</template>
